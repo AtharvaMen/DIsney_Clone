e6dc32d5239af278e2538872a4be0a7a1394211e
AtharvaMen <atharvamen@gmail.com>
2024-08-07 22:26:48 +0530

    pages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import logo from "./../assets/images/logo.png";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiPlus,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
 
 const Header = () => {
+  const menu = [
+    {
+      name: "Home",
+      icon: HiHome,
+    },
+    {
+      name: "Search",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "Watch List",
+      icon: HiPlus,
+    },
+    {
+      name: "Originals",
+      icon: HiStar,
+    },
+    {
+      name: "Movies",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "Series",
+      icon: HiTv,
+    }
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+    </div>
+  );
+};
 
-export default Header
+export default Header;

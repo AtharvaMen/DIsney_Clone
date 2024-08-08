714680bd1a1ad8746717ab259d86f3e84be8dfff
AtharvaMen <atharvamen@gmail.com>
2024-08-08 19:24:41 +0530

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React,{ useState } from "react";
 import logo from "./../assets/images/logo.png";
 import {
   HiHome,
   HiMagnifyingGlass,
   HiStar,
   HiPlayCircle,
-  HiTv
+  HiTv,
 } from "react-icons/hi2";
-import { HiPlus, HiDotsVertical} from "react-icons/hi";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+  const [toggle,setToggle] = useState(false)
   const menu = [
     {
       name: "Home",
@@ -35,17 +36,37 @@ const Header = () => {
     {
       name: "Series",
       icon: HiTv,
-    }
+    },
   ];
   return (
     <div className="flex items-center justify-between">
       <div className="flex  gap-8">
-      <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
-      {menu.map((item) => (
-        <HeaderItem name={item.name} Icon = {item.icon} />
-      ))}
+        <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, index) =>
+              index < 3 && (<HeaderItem name={""} Icon={item.icon} />
+          ))}
+          <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle? <div className="absolute mt-3 bg-[#121212] border-[1px] p-3 border-gray-700 px-5 py-4">
+              {menu.map(
+                (item, index) =>
+                  index > 2 && (<HeaderItem name={item.name} Icon={item.icon} />
+              ))}
+            </div>:null}
+          </div>
+        </div>
       </div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEMAdBlaRt3h9qWKq7Gy06igBGQwpFM2VEA&s"  className="w-[40px] rounded-full"/>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEMAdBlaRt3h9qWKq7Gy06igBGQwpFM2VEA&s"
+        className="w-[40px] rounded-full"
+      />
     </div>
   );
 };
